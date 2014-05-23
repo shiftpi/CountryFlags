34f5b9825e9ac4a29faa3a55bde866b671746943
Andreas Rutz <andreas.rutz@posteo.de>
2014-05-24 00:14:39 +0200

    Moved Module.php, extendend composer.json

--- Module.php
+++ Module.php
@@ -1,2 +1,28 @@
 <?php
-require_once __DIR__ . '/src/ShiftpiCountryFlags/Module.php';
+namespace ShiftpiCountryFlags;
+
+use Zend\ModuleManager\Feature\ConfigProviderInterface;
+
+/**
+ * Module class
+ * @author Andreas Rutz <andreas.rutz@posteo.de>
+ * @license MIT
+ */
+class Module implements ConfigProviderInterface
+{
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+}
