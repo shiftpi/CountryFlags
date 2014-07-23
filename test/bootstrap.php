b84bcc1fe13b7767767dcc55de1d07ec5b4d07d7
Andreas Rutz <andreas.rutz@posteo.de>
2014-07-23 20:06:13 +0200

    Made testing bootstrap more flexible

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,2 +1,15 @@
 <?php
-require_once __DIR__ . '/../../../autoload.php';
+$autoloadPath =  __DIR__ . '/../../../vendor/autoload.php';
+if (file_exists($autoloadPath)) {           // If the module is installed under /module
+    require $autoloadPath;
+} else {                                    // else it is installed under /vendor
+    require __DIR__ . '/../../../autoload.php';
+}
+
+$loader = new \Zend\Loader\StandardAutoloader(array(
+    \Zend\Loader\StandardAutoloader::LOAD_NS => array(
+        'ShiftpiCountryFlags' => __DIR__ . '/../src/ShiftpiCountryFlags',
+    ),
+));
+
+$loader->register();
